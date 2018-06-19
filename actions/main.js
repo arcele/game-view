1de1527d048259e2cf002562eefbefbaee7e8a1c
Ryan Kelley <ryan.kelley@espn.com>
2018-06-19 13:46:23 -0400

    fetch probable data from the mlb api

--- actions/main.js
+++ actions/main.js
@@ -1,4 +1,4 @@
-import { SAVE_SCHEDULE, SAVE_GAME } from '../types/main'
+import { SAVE_SCHEDULE, SAVE_GAME, SAVE_PROBABLE_STARTERS } from '../types/main'
 import fetch from 'isomorphic-fetch'
 
 
@@ -14,16 +14,10 @@ export const savePlayer = (player) => {
 }
 
 export const fetchSchedule = () => {
-	// check if we have proGames before we do this tho.
-
-	// mlb schedule: http://lookup-service-prod.mlb.com/json/named.mlb_broadcast_info.bam?src_type='TV'&tcid=mm_mlb_schedule&sort_by='game_time_et_asc'&home_away='H'&start_date='20180619'&end_date='20180619'&season='2018'
-
 	const gameDate = new Date().toISOString().split('T')[0].replace(/-/g,'')
 	const gameYear = new Date().getFullYear()
 	const scheduleApi = `http://lookup-service-prod.mlb.com/json/named.mlb_broadcast_info.bam?src_type='TV'&tcid=mm_mlb_schedule&sort_by='game_time_et_asc'&home_away='H'&start_date='${gameDate}'&end_date='${gameDate}'&season='${gameYear}'`
-	console.log('lets grab:', scheduleApi)
 	return dispatch => {
-		//fetch('http://games.espn.com/flb/api/v2/proGames?scoringPeriodId=83').then((response) => {
 		fetch(scheduleApi).then((response) => {
 			return response.json()
 		}).then((resJson) => {
@@ -33,18 +27,19 @@ export const fetchSchedule = () => {
 }
 
 export const fetchGame = (id) => {
-	// potential game feeds:
-	// http://www.espn.com/mlb/game?gameId=380619122&xhr=1
-
 	return dispatch => {
-		fetch(`http://site.api.espn.com/apis/site/v2/sports/baseball/leagues/mlb/gameheader?event=${id}&lang=en&contentOrigin=espn`).then((res) => {
+		const probableApi = `https://statsapi.mlb.com/api/v1/schedule?gamePk=${id}&language=en&hydrate=lineups,broadcasts(all),probablePitcher(note),game(tickets)&useLatestGames=true&fields=dates,games,teams,probablePitcher,note,id,dates,games,broadcasts,type,name,homeAway,isNational,dates,games,game,tickets,ticketType,ticketLinks,dates,games,lineups,homePlayers,awayPlayers,useName,lastName,primaryPosition,abbreviation`
+		fetch(probableApi).then((res) => {
 			return res.json()
-		}).then((resJson) => {
-			dispatch({type: SAVE_GAME, game: resJson})
-			// we need to use mlb feeds instead of these espn ones.
-			// now get bvp data from here? http://lookup-service-prod.mlb.com/json/named.stats_batter_vs_pitcher_composed.bam?sport_code=%27mlb%27&game_type=%27R%27&pitcher_id=592826&team_id=138&season=2018
-			console.log('can i get a playerId and a team id...138? wtf', resJson)
+		}).then((probableData) => {
+			const away = probableData.dates[0].games[0].teams.away.probablePitcher.id
+			const home = probableData.dates[0].games[0].teams.home.probablePitcher.id
+			const awayTeam =  probableData.dates[0].games[0].teams.away
+			console.log('probableData:', probableData)
+			dispatch({type: SAVE_PROBABLE_STARTERS, starters: {away, home}})
+
+			const bvpApi = `https://lookup-service-prod.mlb.com/json/named.team_bvp_5y.bam?vs_pitcher_id=${home}&game_type=%27R%27&team_id=${awayTeam.id}&year=2018`
+			console.log('hit:', bvpApi)
 		})
 	}
 }
-
