7f0669e7a9459e7d2cd755b80206d7cec103f9c5
Ryan Kelley <ryan.kelley@espn.com>
2018-06-19 09:33:05 -0400

    lil styling for the matchup

--- containers/Matchup.js
+++ containers/Matchup.js
@@ -13,12 +13,46 @@ class Matchup extends Component {
 		const awayTeam = game && game.competitors ? game.competitors[1] : null
 		const homeTeam = game && game.competitors ? game.competitors[0] : null
 		return (
-			<div>
-				<p>
-					{ awayTeam && awayTeam.team.abbreviation } 
+			<div style={{width:"300px"}}>
+				
+						
+					<div className="team away" style={ { float:"left", width:"50%" } }>	
+						{ awayTeam && awayTeam.team.abbreviation }
+						<div style={
+							{
+								float:"left",
+								width:"60px",
+								height:"60px",
+								backgroundImage:awayTeam && awayTeam.team.logos ? `url(${awayTeam.team.logos[3].href})` : 'url(n)',
+								backgroundPosition: "5px 5px",
+								backgroundSize:"50px",
+								backgroundColor: awayTeam && awayTeam.team ? '#' + awayTeam.team.color : 'black',
+//								borderColor: awayTeam && awayTeam.team ? '#' + awayTeam.team.secondaryColor : 'grey',
+								border:"2px solid #" + (awayTeam && awayTeam.team ? awayTeam.team.alternateColor : '333'),
+								backgroundRepeat:"no-repeat",
+								borderRadius:"30px"
+							}	
+						}>&nbsp;</div>
+					</div>
 					@ 
-					{ homeTeam && homeTeam.team.abbreviation }
-				</p>
+					<div className="team home" style={ { float:"left", width:"50%" } }>	
+						
+						<div style = {
+							{
+								float:"right",
+								width:"60px",
+								height:"60px",
+								backgroundImage:homeTeam && homeTeam.team.logos ? `url(${homeTeam.team.logos[3].href})` : 'url()',
+								backgroundPosition: "5px 5px",
+								backgroundSize:"50px",
+								backgroundColor: homeTeam && homeTeam.team ? '#' + homeTeam.team.color : 'black',
+								backgroundRepeat:"no-repeat",
+								borderRadius:"30px",
+								border:"2px solid #" + (homeTeam && homeTeam.team ? homeTeam.team.alternateColor : '333'),
+							}
+						}>&nbsp;</div>
+						{ homeTeam && homeTeam.team.abbreviation }
+					</div>
 				<Pitcher team={awayTeam} />
 				<Batters team={homeTeam} />
 				<Pitcher team={homeTeam} />
