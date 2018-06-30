e3856b0ff9210847d82c74ab1d1db2a7b0cf2bc5
Ryan Kelley <ryan.kelley@espn.com>
2018-06-29 21:18:13 -0400

    fetch and save bvp data

--- actions/main.js
+++ actions/main.js
@@ -1,4 +1,4 @@
-import { SAVE_SCHEDULE, SAVE_GAME, SAVE_PROBABLE_STARTERS, REQUEST_SCHEDULE } from '../types/main'
+import { SAVE_SCHEDULE, SAVE_GAME, SAVE_PROBABLE_STARTERS, REQUEST_SCHEDULE, SAVE_BVP_DATA } from '../types/main'
 import fetch from 'isomorphic-fetch'
 
 
@@ -37,25 +37,28 @@ export const fetchSchedule = () => {
 	}
 }
 
-export const fetchGame = (id) => {
-	// we need to fetch the entire schedule if we don't ahve it first
+export const fetchGame = (game) => {
 	return dispatch => {
+		const id = game['game_pk']
 		const probableApi = `https://statsapi.mlb.com/api/v1/schedule?gamePk=${id}&language=en&hydrate=lineups,broadcasts(all),probablePitcher(note),game(tickets)&useLatestGames=true&fields=dates,games,teams,probablePitcher,note,id,dates,games,broadcasts,type,name,homeAway,isNational,dates,games,game,tickets,ticketType,ticketLinks,dates,games,lineups,homePlayers,awayPlayers,useName,lastName,primaryPosition,abbreviation`
-
-		// fetch returns a promise, so, let's make a couple of fetches and then do a Promise.all?
-
-		// this fetch gets and saves the probable pitchers for the game, we'll need to get the
-		// opposing teams bvp data using the bvpApi for both teams.
 		fetch(probableApi).then((res) => {
 			return res.json()
 		}).then((probableData) => {
 			const away = probableData.dates[0].games[0].teams.away.probablePitcher.id
 			const home = probableData.dates[0].games[0].teams.home.probablePitcher.id
-			const awayTeam =  probableData.dates[0].games[0].teams.away
-			console.log('probableData:', probableData)
 			dispatch({type: SAVE_PROBABLE_STARTERS, gameId: id, starters: {away, home}})
-//			const bvpApi = `https://lookup-service-prod.mlb.com/json/named.team_bvp_5y.bam?vs_pitcher_id=${home}&game_type=%27R%27&team_id=${awayTeam.id}&year=2018`
-//			console.log('hit:', bvpApi)
+			const homePitcherApi = `https://lookup-service-prod.mlb.com/json/named.team_bvp_5y.bam?vs_pitcher_id=${home}&game_type=%27R%27&team_id=${game['away_team_id']}&year=2018`
+			const awayPitcherApi = `https://lookup-service-prod.mlb.com/json/named.team_bvp_5y.bam?vs_pitcher_id=${away}&game_type=%27R%27&team_id=${game['home_team_id']}&year=2018`
+			fetch(homePitcherApi).then((res) => {
+				return res.json()
+			}).then((homeData) => {
+				dispatch({type: SAVE_BVP_DATA, pitcher: home, data: homeData})
+				fetch(awayPitcherApi).then((awayRes) => {
+					return awayRes.json()
+				}).then((awayData) => {
+					dispatch({type: SAVE_BVP_DATA, pitcher: away, data: awayData})
+				})
+			})
 		})
 
 		const gameApi = `https://statsapi.mlb.com/api/v1.1/game/${id}/feed/live?language=en`
--- reducers/main.js
+++ reducers/main.js
@@ -1,4 +1,4 @@
-import { SAVE_SCHEDULE, LOAD_GAME, SAVE_PROBABLE_STARTERS, REQUEST_SCHEDULE } from '../types/main'
+import { SAVE_SCHEDULE, LOAD_GAME, SAVE_PROBABLE_STARTERS, REQUEST_SCHEDULE, SAVE_BVP_DATA } from '../types/main'
 import { combineReducers } from 'redux'
 
 const initState = {
@@ -10,6 +10,17 @@ const schedule = (state = {
 		game: undefined,	// the currently in view game
 		requestedGames: false,
 	}, action) => {
+	
+	// Simple function to grab a game by id from the list of proGames
+	const getGame = (gameId) => (
+		state.proGames.find(g => g['game_pk'] === gameId)
+	)
+
+	// Return the currently loaded game
+	const getCurrentGame = () => (
+		getGame(state.game['game_pk'])
+	)
+
 	switch (action.type) {
 		case REQUEST_SCHEDULE:
 			// Set a flag upon initial request of the schedule to make sure we dont' request it again
@@ -28,15 +39,28 @@ const schedule = (state = {
 				// This creates a reference to the game, which is rad for reading,
 				// but, you have to write any changes to the og game in proGames or, wtf
 				// was any of this even for?!??!?!?!??! :creed: :scuba:
-				game: state.proGames.find((x) => x['game_pk'] === gameId)
+				game: getGame(gameId)
 			})
 		case SAVE_PROBABLE_STARTERS:
 			// Save the probable starters to the original game element
 			const proGames = state.proGames.slice(0)
-			const game = proGames.find((x) => x['game_pk'] === action.gameId)
+			let game = getGame(action.gameId)
 			game.starters = action.starters
 			return Object.assign({}, state, {
-				proGames: proGames
+				proGames
+			})
+		case SAVE_BVP_DATA:
+			// Save the BVP data to the state
+			game = getCurrentGame()
+			console.log('saving for game, okay? :', game)
+			if(game.starters && game.starters.home === action.pitcher) {
+				game.starters['home_pitcher_bvp'] = action.data.team_bvp_5y.queryResults.row
+			}
+			if(game.starters && game.starters.away === action.pitcher) {
+				game.starters['away_pitcher_bvp'] = action.data.team_bvp_5y.queryResults.row
+			}
+			return Object.assign({}, state, {
+				game
 			})
 		default:
 			return state
