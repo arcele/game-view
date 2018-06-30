906846927e4369d8dc18d60f9e4be04019759e02
Ryan Kelley <ryan.kelley@espn.com>
2018-06-30 00:05:27 -0400

    display some bvp data

--- components/Batters.js
+++ components/Batters.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react'
 
 class Batters extends Component {
 	render() {
+		console.log('batters props:', this.props)
+		const players = this.props.players || []
+		console.log('players tho:', players)
 		return (
-			<p>Batters Component</p>
+			<div>
+				<p>Batters Component</p>
+				<table>
+					<thead>
+						<tr>
+							<td>&nbsp;</td>
+							<td>H/AB</td>
+							<td>AVG</td>
+							<td>HR</td>
+							<td>SLG</td>
+						</tr>
+					</thead>
+					<tbody>
+					{ players.map((player) => 
+						player && player.b_tpa ? (<tr>
+							<td>{player.name}</td>
+							<td>{player.b_total_hits} / { player.b_ab}</td>
+							<td>{player.b_batting_avg}</td>
+							<td>{player.b_home_run}</td>
+							<td>{player.b_slugging_avg}</td>
+						</tr>) : (<tr><td>{player.name}</td><td colspan="4">No Stats</td></tr>)
+					)}
+					</tbody>
+				</table>
+			</div>
 		)
 	}
 }
