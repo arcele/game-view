bc4483cdff992608a2e0cfa6d068d509cd974bd0
Ryan Kelley <ryan.kelley@espn.com>
2018-06-30 14:43:00 -0400

    just don't print guys with no stats

--- components/Batters.js
+++ components/Batters.js
@@ -18,13 +18,13 @@ class Batters extends Component {
 					</thead>
 					<tbody>
 					{ players.map((player) => 
-						player && player.b_tpa ? (<tr>
+						player && player.b_tpa && (<tr>
 							<td>{player.name}</td>
 							<td>{player.b_total_hits} / { player.b_ab}</td>
 							<td>{player.b_batting_avg}</td>
 							<td>{player.b_home_run}</td>
 							<td>{player.b_slugging_avg}</td>
-						</tr>) : (<tr><td>{player.name}</td><td colspan="4">No Stats</td></tr>)
+						</tr>)
 					)}
 					</tbody>
 				</table>
