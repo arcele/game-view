25db0dcc2b280fae9111deb67d2f9cdbd5d51d31
Ryan Kelley <arcele@gmail.com>
2019-05-25 20:31:22 -0400

    add all site odds to odds component

--- components/Odds.js
+++ components/Odds.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import Tooltip from '@material-ui/core/Tooltip';
 
 class Odds extends Component {
   render() {
     let data = this.props.odds,
-        format = this.props.format ? this.props.format : 'us'; //default to us
-    console.log('Odds Data:', data, this.props)
+        format = this.props.format ? this.props.format : 'us', //default to us
+        toolTipText = ''
+        this.props.odds && this.props.odds.sites && this.props.odds.sites.forEach((site) => {
+          toolTipText += `${site.site_key} : ${site.odds.us} \n`
+        })
     if(data) {
       return(
-          <span className="odds" data-euro-rounded={Math.round(data.eur)}>
-            { `(${data[format]})` }
-          </span>
+        <Tooltip title={toolTipText}>
+            <span className="odds" data-euro-rounded={Math.round(data.eur)}>
+                { `(${data[format]})` }
+            </span>
+        </Tooltip>
       )
     } else {
       return <span className="odds" />
