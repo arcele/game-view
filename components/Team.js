68e1a176f2e2427eb63e143641db9dc825f8c008
Ryan Kelley <arcele@gmail.com>
2019-06-18 20:20:32 -0400

    more complete team standings

--- components/Team.js
+++ components/Team.js
@@ -7,24 +7,38 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import _ from 'lodash'
 
 class Team extends Component {
   render() {
-    let team = this.props.team;
-    let standings = this.getTeamStandings();
-    let rowClass = ''
+    let team = this.props.team,
+        standings = this.getTeamStandings(),
+        rowClass = '',
+        splitRecords,
+        lastTen;
+
     return(
       <React.Fragment>
         <Grid container spacing={1}>
           <Grid item xs={3}>
             <img className="teamLogo" style={{height:75}} src={team && "https://www.mlbstatic.com/team-logos/" + team.id + ".svg"} />
+            <Odds odds={team && team.odds} format='us' />
           </Grid>
           <Grid item xs={9}>
             <Table className="standings">
+              <TableHead>
+                <TableRow>
+                  <TableCell>&nbsp;</TableCell>
+                  <TableCell>Rec</TableCell>
+                  <TableCell>L10</TableCell>
+                  <TableCell>Str</TableCell>
+                </TableRow>
+              </TableHead>
               <TableBody>
               { standings && standings.teamRecords && standings.teamRecords.map((sTeam) => {
-                console.log(team.id, sTeam.team.id)
                   rowClass = (team.id === sTeam.team.id) ? 'thisTeam' : 'team'
+                  splitRecords = sTeam.records.splitRecords
+                  lastTen = _.find(splitRecords, {type: "lastTen"})
                   return(
                     <TableRow className={rowClass}>
                       <TableCell>
@@ -33,7 +47,12 @@ class Team extends Component {
                       <TableCell>
                         <span className="record">{sTeam.leagueRecord.wins}-{sTeam.leagueRecord.losses}</span>
                       </TableCell>
-                    
+                      <TableCell>
+                        <span className="record">{lastTen.wins}-{lastTen.losses}</span>
+                      </TableCell>
+                      <TableCell>
+                        <span className="record">{sTeam.streak.streakCode}</span>
+                      </TableCell>
                     </TableRow>
                   )
                 })
